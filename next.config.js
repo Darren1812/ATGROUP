5c6928b1147a43132cee97c20ad819e53aa460cb
Darren1812 <darrenwong512@gmail.com>
2026-03-13 12:33:23 +0800

    update somethings

--- next.config.js
+++ next.config.js
@@ -1,7 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["via.placeholder.com", "192.168.1.200"],
+    // 允许通过代理加载外部图片
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'atgroup.synology.me',
+        port: '5003',
+        pathname: '/api/PrinteImage/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+        pathname: '/**',
+      },
+      // 如果你还需要本地调试，保留这个
+      {
+        protocol: 'http',
+        hostname: '192.168.1.200',
+        port: '5002', 
+        pathname: '/api/PrinteImage/**',
+      }
+    ],
   },
 }
 
